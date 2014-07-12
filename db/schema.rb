0cf34be66e5a679faea76c327e157681859a3e60
se-yoshi <se.yoshi@gmail.com>
2014-07-12 18:29:31 +0900

    ユーザ情報からemailを削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706125631) do
+ActiveRecord::Schema.define(version: 20140712082153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20140706125631) do
   add_index "user_schedule_relations", ["user_id"], name: "index_user_schedule_relations_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -67,7 +66,6 @@ ActiveRecord::Schema.define(version: 20140706125631) do
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["login_name"], name: "index_users_on_login_name", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
